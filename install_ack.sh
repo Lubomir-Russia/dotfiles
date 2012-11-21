2d9a7d35160f7a4b4777bf687a446307b62a1e7e
Lubomir <Lubomir@mars.com>
2012-11-21 19:42:01 +0000

    Fix install_ack.sh

--- install_ack.sh
+++ install_ack.sh
@@ -1,2 +1,2 @@
 echo Installing ack from BetterThanGrep
-curl http://betterthangrep.com/ack-standalone > ~/bin/ack && chmod 0755 !#:3
+curl http://betterthangrep.com/ack-standalone > ~/bin/ack && chmod 0755 ~/bin/ack
